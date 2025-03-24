b140630b4806c6e51e42a0b9ffb3812703b2be97
André Leal Juns <110703970+AndreSLeal1@users.noreply.github.com>
2025-03-24 09:54:49 -0300

    Update tasksRepository.java

--- src/main/java/com/projeto/springdb/repository/tasksRepository.java
+++ src/main/java/com/projeto/springdb/repository/tasksRepository.java
@@ -1,13 +1,10 @@
-
-
 package com.projeto.springdb.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.projeto.springdb.entities.tasks;
-
 import java.util.UUID;
+
 @Repository
 public interface tasksRepository extends JpaRepository<tasks, UUID> {
 
